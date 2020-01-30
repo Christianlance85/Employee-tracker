2106db6771099e322179091339b4a957467dee00
Christian <christianlance85@gmail.com>
2020-01-30 16:55:48 -0600

    removing credentials

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "christianlance",
-  password: "some_pass",
+  user: "root",
+  password: "",
   database: "employeeTracker_db"
 });
 
